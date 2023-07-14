8e13e06d7b15c6e5d79efd4e6f8d907d2029e23a
Vika Zhuravel <aksenova335@gmail.com>
2023-07-14 14:55:59 +0300

    this my first commit

---
+++ constants/catalog.js
@@ -0,0 +1,21 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'ORBEA MX 50 29"',
+        img: '',
+        price: 23292,
+    },
+    {
+        id: 'el2',
+        name: 'PRIDE MARVEL 7.2 MS',
+        img: '',
+        price: 14500,
+    },
+    {
+        id: 'el3',
+        name: 'MARIN BOLINAS RIDGE 1 29"',
+        img: '',
+        price: 21560,
+    }
+    
+]
