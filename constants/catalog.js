12da88f753f8c7d9da1cdaf3e0135b46030c1983
Vika-Zhu <aksenova335@gmail.com>
2023-07-14 16:04:18 +0300

    added img to the catalog

--- constants/catalog.js
+++ constants/catalog.js
@@ -2,19 +2,19 @@ const CATALOG = [
     {
         id: 'el1',
         name: 'ORBEA MX 50 29"',
-        img: '',
+        img: 'https://github.com/Vika-Zhu/bike_shop/blob/master/image/catalog/ORBEA%20_MX%20_50.jpeg',
         price: 23292,
     },
     {
         id: 'el2',
         name: 'PRIDE MARVEL 7.2 MS',
-        img: '',
+        img: 'https://github.com/Vika-Zhu/bike_shop/blob/master/image/catalog/PRIDE%20_MARVEL.jpeg',
         price: 14500,
     },
     {
         id: 'el3',
         name: 'MARIN BOLINAS RIDGE 1 29"',
-        img: '',
+        img: 'https://github.com/Vika-Zhu/bike_shop/blob/master/image/catalog/MARIN_BOLINAS_RIDGE.jpeg',
         price: 21560,
     }
     
