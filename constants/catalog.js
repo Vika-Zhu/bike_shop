c45d454d34e20fcc6ee67db0c85c49abffa58722
Vika-Zhu <aksenova335@gmail.com>
2023-07-14 23:34:45 +0300

    added oll  image to the catalog

--- constants/catalog.js
+++ constants/catalog.js
@@ -16,6 +16,62 @@ const CATALOG = [
         name: 'MARIN BOLINAS RIDGE 1 29"',
         img: 'https://github.com/Vika-Zhu/bike_shop/blob/master/image/catalog/MARIN_BOLINAS_RIDGE.jpeg',
         price: 21560,
-    }
+    },
+    {
+        id: 'el4',
+        name: 'CANNONDALE TRAIL 8 29"',
+        img: '',
+        price: 21600,
+    },
+    {
+        id: 'el5',
+        name: 'MARIN NICASIO+"',
+        img: '',
+        price: 35326,
+    },
+    {
+        id: 'el6',
+        name: 'PRIDE MARVEL 7.1',
+        img: '',
+        price: 14600,
+    },
+    {
+        id: 'el7',
+        name: 'MARIN KENTFIELD 1',
+        img: '',
+        price: 20760,
+    },
+    {
+        id: 'el8',
+        name: 'Marin Bolinas Ridge 1 29"',
+        img: '',
+        price: 21560,
+    },
+    {
+        id: 'GT AGGRESSOR EXPERT MS 27.5"',
+        name: '',
+        img: '',
+        price: 24800,
+    },
+    {
+        id: 'el10',
+        name: 'PRIDE STELLA 6.1 MS',
+        img: '',
+        price: 11526,
+    },
+    {
+        id: 'el11',
+        name: 'TRINX M100 26"',
+        img: '',
+        price: 12630,
+    },
+    {
+        id: 'el12',
+        name: 'PRIDE SPROCKET 8.1',
+        img: '',
+        price: 19800,
+    },
+
+
     
 ]
